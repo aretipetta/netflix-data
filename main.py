d9a9a19decebcc5662c9b9405d3ab3bbbe8917bf
areti <aretpett@gmail.com>
2024-02-16 00:26:45 +0200

    added feature to print data properly

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import sys
 import pandas as pd
 import re
 
-from results.print_results import print_results
+from results.print_results import print_the_first_movie_or_series_per_profile
+from results.print_results import print_profiles_data_per_year_y
 
 
 # ------ The following method returns the data from a dataframe of a specified profile for a certain year
@@ -22,11 +23,19 @@ def modify_dataframe(dataframe):
     modified_df = pd.DataFrame(columns=['Profile Name', 'Title', 'Category', 'Datetime'])
     for index, row in dataframe.iterrows():
         # Group movies/series
-        split_pattern = re.compile(r': Season \d+ :')   # identify the series
-        title = re.split(split_pattern, row['Title'])
-        category = 'Movie' if len(title) == 1 else 'TV Show'
 
-        modified_df.loc[index] = [row["Profile Name"], title[0], category, row['Start Time'].split(' ')[0]]
+        # todo: check again the \d+
+        # split_pattern = re.compile(r': Season \d+ :')  # identify the series
+        # title = re.split(split_pattern, row['Title'])
+        # category = 'Movie' if len(title) == 1 else 'TV Show'
+
+        # the search method returns an object with two items;
+        # a tuple object for the start and end index of the successful match
+        # and an actual matching value that can be retrieved using the group() method
+        match = re.search(r': Season ', row['Title'])
+        category = 'TV Show' if match else 'Movie'
+
+        modified_df.loc[index] = [row["Profile Name"], row['Title'], category, row['Start Time'].split(' ')[0]]
 
     return modified_df
 
@@ -44,17 +53,35 @@ def read_file(filename):
 
     modified_data = modify_dataframe(df)
 
+    array_of_dataframes_per_profile = []
+    print('----------------------------------------------------------------------------------------')
+    print('\n########### The very first watched movie/series for each user profile ###########\n')
     for profile in distinct_profile_names:
-        print('---------------------------------------------------------------------')
         print('-----> User ', profile)
-        data = data_per_profile_name(profile, modified_data)
-        temp_df = pd.DataFrame(columns=years)
+        data_for_profile = data_per_profile_name(profile, modified_data)
+
+        # ---- keep the dataframe for each 'profile' in an array
+        array_of_dataframes_per_profile.append(data_for_profile)
+
+        # Print the first watched movie/series
+        print_the_first_movie_or_series_per_profile(data_for_profile, profile)
+
+    print('\n########### All the watched movie/series for each user profile  per year ###########\n')
+    idx = 0
+    for profile in distinct_profile_names:
+        # Get the dataframe for this profile based on the indexing
+        df = array_of_dataframes_per_profile[idx]
         for y in years:
-            dt = data_per_profile_name_and_year(data, y)
-            # temp_df[y] = dt
-            print('--- year: ', y)
-            print(dt)
-        # print(temp_df)
+            print('------------------------------------------------------')
+            # Get the data for this year
+            data_for_year_y = data_per_profile_name_and_year(df, y)
+            print('-----> User profile: ', profile, ' for Year: ', y)
+            # Print all the data for this 'profile' for the year 'y'
+            print_profiles_data_per_year_y(data_for_year_y)
+            # print(data_for_year_y)
+        idx += 1  # move to the next profile, so it can get the corresponding dataframe from array
+        print('------------------------------------------------------')
+
 
 if __name__ == '__main__':
     # args = sys.argv[1:]
--- results/print_results.py
+++ results/print_results.py
@@ -1,3 +1,22 @@
+# ------ The following method will print the very first watched movie/series for each profile
+def print_the_first_movie_or_series_per_profile(dataframe_for_profile, profile_name):
+    # tail() method by default returns the 5 last rows of a dataframe
+    first_movie_name = dataframe_for_profile['Title'].tail(1).to_string().split('    ')[1]
+    date_of_first_watch = dataframe_for_profile['Datetime'].tail(1).to_string().split('    ')[1]
+    print('     The very first movie/series for user', profile_name, ':')
+    print('         Title: ', first_movie_name)
+    print('         Date: ', date_of_first_watch)
 
-def print_results():
-    print("hello")
+
+# ------ The following method will print all the data for a certain profile and year
+#       It prints the title, the category and the date of the watched movie/series
+def print_profiles_data_per_year_y(dataframe_for_profile_and_year):
+    # Iterate through rows
+    for row_idx in range(len(dataframe_for_profile_and_year)):
+        title = dataframe_for_profile_and_year.iloc[row_idx, 0]   # 0 ==> 'Title'
+        category = dataframe_for_profile_and_year.iloc[row_idx, 1]    # 1 ==> 'Category'
+        date = dataframe_for_profile_and_year.iloc[row_idx, 2]    # 2 ==> 'Datetime'
+        print('    Details for movie/category #', row_idx + 1)
+        print('             Title: ', title)
+        print('          Category: ', category)
+        print('              Date: ', date)
